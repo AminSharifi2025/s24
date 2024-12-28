0ab2d771b0ce4556b6d195ab565fb8e26e9d80a5
Amin Sharifi <asharifi4@gmail.com>
2024-12-28 10:18:19 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+import HelloWorld from "./components/HelloWorld.vue";
+// export default{
+//   setup:()=>{
+    const message ="Amin";
+    const name =ref("");
+//     return{
+//       message,
+//     };
+//   },
+// };
+</script>
+
+<template>
+<HelloWorld/>
+  <input v-model="name" type="text">
+
+  <div class="hi">hi{{ name }}</div>
+</template>
+
+
+
+<style>
+.hi{
+  border: solid 1px red;
+}
+</style>
